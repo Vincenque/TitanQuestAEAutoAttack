5782cdafe056404c210508be344d8688f1cf4ab8
V-Tec <dudawitold.dw@gmail.com>
2023-08-30 13:25:38 +0200

    Changed pixel which script is checking and created simple messeages about script state

--- TitanQuestAEAutoAttack.py
+++ TitanQuestAEAutoAttack.py
@@ -11,12 +11,20 @@ def click(x,y):
     time.sleep(0.01)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
+scriptOffMesseagePrinted = True
+print("Press 'O' to turn script on")
 while True:
     keyState = win32api.GetKeyState(79) #79 = O
+    if keyState == 1:
+        print("Script on")
+    elif keyState != 1 and scriptOffMesseagePrinted == False:
+        print("Script off")
+        scriptOffMesseagePrinted = True
     while keyState == 1:
+        scriptOffMesseagePrinted = False
         keyState = win32api.GetKeyState(79) #79 = O
-        #print(pyautogui.pixel(953,65)[0])
-        if pyautogui.pixel(953,65)[0]==248 or pyautogui.pixel(953,65)[0]==90:
+        #print(pyautogui.pixel(905,65))
+        if pyautogui.pixel(905,65)[0]>200:
             win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
             time.sleep(0.01)
             win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
